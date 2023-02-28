5793abdb62e60fa2a85ba6c014acb5330319fff3
burulai2003 <burulaitursunbaeva33@gmail.com>
2023-02-28 18:15:53 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// var jsonData = {
+//     JSON
+// }
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const error = false
+        if (!error) {
+            const jsonData = {
+                userName: "Burulai",
+                userAge: 19,
+                adress: {
+                    city: "Osh",
+                    street: "Kygystan"
+                }
+            }
+            const jsonstringify = JSON.stringify(jsonData)
+            console.log(jsonstringify);
+            resolve(jsonstringify)
+        } else {
+            reject('something has gone wrong')
+        }
+    }, 2000)
+
+});
+promise
+    .then((jsonData) => {
+        setTimeout(() => {
+            const parse = JSON.parse(jsonData)
+            console.log(parse);
+        }, 2000)
+    })
+    .catch((error) => {
+        console.log('Oops sorry ,try again', error);
+    })
+    .finally(() => {
+        console.log('loading ended');
+    })
